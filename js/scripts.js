3c7fc5f1473f60698289197a6f2e039051dc4079
andrico1234 <andrico1234@yahoo.co.uk>
2016-04-29 13:23:02 +0100

    v0.4
    - Started CC functionality
    - Added progress bar functionality
    - Added fullscreen, play, pause and other buttons
    - No time stamp
    - No buffering progress
    - No hover implementation
    - No transcript effect

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,24 @@
  * Created by Home on 28/04/2016.
  */
 
-video = document.getElementById("main-video");
-currentVolume = video.volume;
+var $video = document.getElementById("main-video");
+var currentVolume = $video.volume;
+var seekSlider = document.getElementById("seek-bar");
+var volumeBar = document.getElementById("volume-fader");
+$video.addEventListener("timeupdate", seekTimeUpdate, false);
+// var subtitles = document.getElementById('subtitles');
 
 // play pause function
 
 function pauseVideo() {
-    video.pause();
+    $video.pause();
     $("#play-button").removeClass("hidden");
     $("#pause-button").addClass("hidden");
     console.log("video paused");
 }
 
 function playVideo() {
-    video.play();
+    $video.play();
     $("#pause-button").removeClass("hidden");
     $("#play-button").addClass("hidden");
     console.log("video played");
@@ -29,10 +33,27 @@ $("#pause-button").click(function () {
     pauseVideo();
 });
 
+// video seek
+
+function videoSeek() {
+    $video.currentTime = $video.duration * (seekSlider.value / 100);
+}
+
+function seekTimeUpdate() {
+    var newTime = $video.currentTime * (100 / $video.duration);
+    seekSlider.value = newTime;
+    console.log("real time update");
+}
+
+$(seekSlider).change(function () {
+    videoSeek();
+    console.log("position changed");
+});
+
 // volume functions
 
 function volumeToggle() {
-    currentVolume = video.volume; // current volume
+    currentVolume = $video.volume; // current volume
     if (currentVolume > 0) {
         $("#volumeOn").removeClass("hidden");
         $("#volumeOff").addClass("hidden");
@@ -44,22 +65,21 @@ function volumeToggle() {
 
 $("#volumeOn").click(function () {
     console.log("volume off");
-    video.volume = 0;
+    $video.volume = 0;
     volumeToggle();
 });
 
 $("#volumeOff").click(function () {
     console.log("volume on");
-    video.volume = 1;
+    $video.volume = 1;
     volumeToggle();
 });
 
+// subtitle code
 
-// toggle volume
-// when volume is 0 hide volume on button
-// when volume is > 0 hide volume off button
-// get current video.volume. set it to currentVolume
-//
+// for (var i = 0; i < $video.textTracks.length; i++) {
+//     $video.textTracks[i].mode = 'hidden';
+// }
 
 // full screen
 
@@ -76,3 +96,4 @@ $("#full-screen").click(function () {
         elem.webkitRequestFullscreen();
     }
 });
+
